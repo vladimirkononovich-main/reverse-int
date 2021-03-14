87ab05b9f56f03f9fccc82809e8d8af87c28091a
vladimirkononovich-main <vova.kononovich.main@gmail.com>
2021-03-14 03:38:12 +0300

    feat: change

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+    n = n < 0 ? n * -1 : n;
+    return Number(
+       n
+        .toString()
+        .split('')
+        .reverse()
+        .join(''));
 }
